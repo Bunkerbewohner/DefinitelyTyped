04be27fe62b7f18343a83da58eec72e3eca90d77
Mathias Kahl <mathias.kahl@krallmann.com>
2015-10-29 12:13:52 +0100

    INIT

---
+++ nativescript/nativescript.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for NativeScript v1.4.0
+// Project: https://www.nativescript.org/
+// Definitions pulled from: https://github.com/NativeScript/NativeScript/releases
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
